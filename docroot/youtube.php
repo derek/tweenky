dac13f9753577d57e3f25e97a7dd0856eb437266
1620 <drgath@gmail.com>
2009-01-17 04:55:36 -0500

    new files

---
+++ docroot/youtube.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	 	<link rel="stylesheet" type="text/css" href="/css/reset.css" />
+
+	</head>
+	<body>
+<object width="820" height="472"><param name="movie" value="http://www.youtube.com/v/<?= $_GET['key'] ?>&hl=en&fs=1&autoplay=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/<?= $_GET['key'] ?>&hl=en&fs=1&autoplay=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="820" height="472"></embed></object>
+</body>
