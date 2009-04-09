f0aad64442d1f292a2a54908d752193544c69884
1620 <drgath@gmail.com>
2009-04-08 23:46:44 -0400

    multiple profiles

--- config.php
+++ config.php
@@ -4,7 +4,17 @@
 
 	session_start();
 
-	define("TWITTER_OAUTH_CONSUMER_KEY", 	"lPkmfsdV4B3d2bLcnkdlg");
-	define("TWITTER_OAUTH_CONSUMER_SECRET", "GPSiu7pGYWcLCfKnSsROfFYrQWwFTP9Mb28Xe9uHcs");
+	if ($_SERVER['HTTP_HOST'] == "ddev.tweenky.com")
+	{
+		define("TWITTER_OAUTH_CONSUMER_KEY", 	"lPkmfsdV4B3d2bLcnkdlg");
+		define("TWITTER_OAUTH_CONSUMER_SECRET", "GPSiu7pGYWcLCfKnSsROfFYrQWwFTP9Mb28Xe9uHcs");
+	}
+	elseif ($_SERVER['HTTP_HOST'] == "new.tweenky.com")
+	{
+		define("TWITTER_OAUTH_CONSUMER_KEY", 	"RgsKsF20AmGfaaB9SQX0Q");
+		define("TWITTER_OAUTH_CONSUMER_SECRET", "53EusUfNkMJKjfc9De1UKN0ischApcUp8hqrLZ1VE");
+	}
+	
+
 
 ?>
