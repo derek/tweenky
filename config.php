b29a8f2ff316bafa39a21f71a7ceb4130c605a47
1620 <drgath@gmail.com>
2009-04-08 23:19:18 -0400

    lots of good updates.  oAuth works now.  Woo\!

---
+++ config.php
@@ -0,0 +1,10 @@
+<?
+
+	require_once('../twitteroauth/twitterOAuth.php');
+
+	session_start();
+
+	define("TWITTER_OAUTH_CONSUMER_KEY", 	"lPkmfsdV4B3d2bLcnkdlg");
+	define("TWITTER_OAUTH_CONSUMER_SECRET", "GPSiu7pGYWcLCfKnSsROfFYrQWwFTP9Mb28Xe9uHcs");
+
+?>
